4812007190141d092b84a5c94006e7e9d35c3b19
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-11-20 09:27:38 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Legend:
+# Note for Self == NFS
+# New Concept = NC
+import flask
+from flask import Flask
+
+
+app = Flask(__name__) # NFS __name__ is a constructor used to by Flask to identify the location of the particle.
