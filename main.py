ac65f34074ec9112b7d242372620adae72455706
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-11-22 12:00:48 +0800

    Signin/up page started

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import flask
+import hashlib
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import UserMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite3'
@@ -13,8 +15,21 @@ db = SQLAlchemy(app)
 @app.route("/")
 def main():
     return render_template('index.html')
-@app.route("/signin")
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    role = db.Column(db.String(10), default="User")
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    email = db.Column(db.String(255), nullable=True, unique=True)
+    password_hash = db.Column(db.String(255), nullable=True)
+    slack_id = db.Column(db.String(255), nullable=True, unique=True)
+
+@app.route("/signin", methods=['GET', 'POST'])
 def signin():
-    return render_template('register.html')
+    return render_template('signin.html')
+    #if request.method=='POST': 
+    #    name=request.get_data()
+
 if __name__ == "__main__":
     app.run(port=4000, debug=True)
