b437e2bbc3a387979bae4f3b32864c4c2afc93bf
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-11-24 22:25:50 +0800

    JS, HTML, CSS Updated for multiple project handling + bug fixes

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -6,26 +6,38 @@ function stop(){
 
 stop();
 
-const checkbox = document.getElementById('review-status');
-const checkbox2 = document.getElementById('shipped-status');
+const reviewCheckbox = document.querySelectorAll('.review-status');
+const shippedCheckbox = document.querySelectorAll('.shipped-status');
 
-checkbox.addEventListener("click", e => e.preventDefault());
-checkbox2.addEventListener("click", e => e.preventDefault());
+reviewCheckbox.forEach(checkbox =>{
+    checkbox.addEventListener("click", (e) => e.preventDefault())
+});
 
-const open = document.getElementById('open');
-const projectBox = document.getElementById('project-box');
-const hidden = document.getElementById('hidden');
-let isOpen = false;
-open.addEventListener("click", ()=>{
-    if (!isOpen){
-        hidden.style.display = "block";
-        projectBox.style.height = "150px";
-        projectBox.style.transition = "all 0.3s ease";
-        isOpen = true;
-    } else {
-        hidden.style.display = "none";
-        projectBox.style.height = "30px";
-        projectBox.style.transition = "all 0.3s ease";
-        isOpen = false;
+shippedCheckbox.forEach(checkbox => {
+    checkbox.addEventListener("click", (e) => e.preventDefault())
+});
+
+const openButtons = document.querySelectorAll(".open");
+
+openButtons.forEach(openButton =>{
+    const projectBox = openButton.closest('.project-box');
+    const hiddenDetails = projectBox ? projectBox.querySelector('.hidden') :  null;
+    let isOpen = false;
+    if (projectBox && hiddenDetails){
+        openButton.addEventListener("click", ()=>{
+            if (!isOpen) {
+                setTimeout(()=>{
+                    hiddenDetails.style.display="block";
+                }, 200);
+                projectBox.style.height = "150px";
+                projectBox.style.transition = "all 0.3s ease";
+                isOpen = true;
+            } else {
+                hiddenDetails.style.display = "none";
+                projectBox.style.height = "30px";
+                projectBox.style.transition = "all 0.3s ease";
+                isOpen = false;
+            }
+        });
     }
-})
+});
